8e06efda36a2aaabb6a181ce60f15d09e44ed9e2
kim <smkim000721@naver.com>
2025-04-18 14:43:28 +0900

    수입/지출 삭제하는 페이지 model 1 -> model 2로 변경

--- cashbook2/src/main/java/controller/DeleteCashController.java
+++ cashbook2/src/main/java/controller/DeleteCashController.java
@@ -1,41 +1,68 @@
 package controller;
 
+import java.io.IOException;
+import java.net.URLEncoder;
+
+import dto.Cash;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import jakarta.servlet.http.HttpSession;
+import model.CashDao;
 
-/**
- * Servlet implementation class DeleteCashController
- */
-@WebServlet("/DeleteCashController")
+@WebServlet("/deleteCash")
 public class DeleteCashController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DeleteCashController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session = request.getSession();
+		String ID = (String)(session.getAttribute("ID"));
+		if(ID == null){ // 로그아웃 상태 일때
+			response.sendRedirect(request.getContextPath()+"/index");
+			return;
+		}	
+		int cashNo = Integer.valueOf(request.getParameter("cashNo"));
+		String kind = request.getParameter("kind");
+		
+		request.setAttribute("cashNo", cashNo);
+		request.setAttribute("kind", kind);
+		
+		request.getRequestDispatcher("/WEB-INF/view/deleteCash.jsp").forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		HttpSession session = request.getSession();
+		String PW = (String)(session.getAttribute("PW"));
+		String pass = request.getParameter("pass");
+		int cashNo = Integer.valueOf(request.getParameter("cashNo"));
+		String kind = request.getParameter("kind");
+		// sendRedirect할때 한글로된 값도 보내기위해 인코딩
+		String encodedKind = URLEncoder.encode(kind, "UTF-8");
+		
+		CashDao cd = new CashDao();
+		Cash c = new Cash();
+		try {
+			c=cd.selectCashDate(cashNo);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		if(PW.equals(pass)){
+			try {
+				cd.deleteCash(cashNo);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			System.out.println("삭제 성공");
+			response.sendRedirect(request.getContextPath()+"/dateList?cashDate="+c.getCashDate());
+		}
+		else{
+			System.out.println("비밀번호가 틀렸습니다");
+			response.sendRedirect(request.getContextPath()+"/deleteCash?cashNo="+cashNo+"&kind="+encodedKind);
+		}
+		
+		
 	}
 
 }
